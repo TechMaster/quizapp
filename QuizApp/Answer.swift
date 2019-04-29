2589f3cabbc5e924213a54b195fe0cb9d8055e23
Techmaster <cuong@techmaster.vn>
2019-04-29 17:00:15 +0700

    1st commit

---
+++ QuizApp/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  QuizApp
+//
+//  Created by Techmaster on 4/29/19.
+//  Copyright © 2019 Techmaster. All rights reserved.
+//
+
+import Foundation
+
+class Answer {
+    
+}
---
+++ QuizApp/Quiz.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  QuizApp
+//
+//  Created by Techmaster on 4/29/19.
+//  Copyright © 2019 Techmaster. All rights reserved.
+//
+
+import Foundation
+class Quiz {
+    
+}
